45f7ea0d8cb614b12d27c3acbc760d3e2640683b
seokho10007 <sukho1007@naver.com>
2023-11-13 15:13:54 +0900

    ✨ feat: 게임 실행을 위한 Model, View, Controller 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,42 @@
+import EventPreviewController from './mvc/controllers/EventPreviewController.js';
+import InputDataController from './mvc/controllers/InputDataController.js';
+
+import EventModel from './mvc/models/EventModel.js';
+
+import InputView from './mvc/views/InputView.js';
+import OutputView from './mvc/views/OutputView.js';
+
 class App {
-  async run() {}
+  #inputView;
+
+  #outputView;
+
+  #eventModel;
+
+  #inputDataController;
+
+  #eventPreviewController;
+
+  constructor() {
+    this.#inputView = InputView;
+    this.#outputView = OutputView;
+    this.#eventModel = new EventModel();
+
+    this.#inputDataController = new InputDataController({
+      inputView: this.#inputView,
+      eventModel: this.#eventModel,
+    });
+
+    this.#eventPreviewController = new EventPreviewController({
+      outputView: this.#outputView,
+      eventModel: this.#eventModel,
+    });
+  }
+
+  async run() {
+    await this.#inputDataController.inputEventData();
+    this.#eventPreviewController.printEventPreview();
+  }
 }
 
 export default App;
